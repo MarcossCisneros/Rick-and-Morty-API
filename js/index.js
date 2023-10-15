d2f6ac32c2f5d11fef0a5498904b036e274519cb
Marcos <marcoscisneros674@gmail.com>
2023-10-14 23:50:19 -0300

    proyecto de api rick y morty

---
+++ js/index.js
@@ -0,0 +1,51 @@
+function getCharacters(data) {
+  const results = fetch("https://rickandmortyapi.com/api/character");
+  results
+    .then((response) => response.json())
+    .then((res) => {
+      data(res);
+    });
+}
+
+getCharacters((data) => {
+  data.results.forEach((personaje) => {
+    const article = document.createElement("article");
+
+    article.innerHTML = `
+    <p class="character__name">
+    ${personaje.name}</p>
+    <img
+      src=${personaje.image}
+      alt=""
+      class="character__img"
+    />
+    <div class="character__details">
+      <p class="character__status">Status: ${personaje.status}</p>
+      <p class="character__species">Species: ${personaje.species}</p>
+      <p class="character__origin">Origin: ${personaje.origin.name}</p>
+    </div>
+    `;
+
+    const container = document.querySelector(".character__container");
+    container.appendChild(article);
+  });
+});
+
+/*
+    <main>
+      <article class="character">
+        <p class="character__name">alberto</p>
+        <img
+          src="https://rickandmortyapi.com/api/character/avatar/492.jpeg"
+          alt=""
+          class="character__img"
+        />
+        <div class="character__details">
+          <p class="character__status">Alive</p>
+          <p class="character__species">Alien</p>
+          <p class="character__origin">Luna</p>
+        </div>
+      </article>
+    </main>
+
+*/
